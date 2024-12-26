8f3ceda9ad8c500e690f17bb9bd825c64b22042c
valrus <ian@mccowan.space>
2024-12-25 17:41:16 -0800

    remove notify kwarg, skip send in add

--- beets/dbcore/db.py
+++ beets/dbcore/db.py
@@ -574,7 +574,7 @@ class Model(ABC, Generic[D]):
 
     # Database interaction (CRUD methods).
 
-    def store(self, fields: Iterable[str] | None = None, notify: bool = False):
+    def store(self, fields: Iterable[str] | None = None):
         """Save the object's metadata into the library database.
         :param fields: the fields to be stored. If not specified, all fields
         will be.
@@ -669,7 +669,7 @@ class Model(ABC, Generic[D]):
             for key in self:
                 if self[key] is not None:
                     self._dirty.add(key)
-            self.store(notify=False)
+            self.store()
 
     # Formatting and templating.
 
